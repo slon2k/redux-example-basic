dab0759366e7e3c2b537d706bf5b812f98429586
Boris Korobeinikov <slon2k@gmail.com>
2019-08-14 16:55:44 +0600

    actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -10,6 +10,19 @@ export const actionTypes = {
     TOGGLE_TODO: 'TOGGLE_TODO'
 };
 
+let nextId = 0;
+
+export const setFilter = (filter) => {
+    return { type: actionTypes.SET_FILTER, filter }
+}
+
+export const addTodo = (text) => {
+    return { type: actionTypes.ADD_TODO, id: nextId++, text }
+}
+
+export const toggleTodo = (id) => {
+    return { type: actionTypes.TOGGLE_TODO, id}
+}
 
 
 
